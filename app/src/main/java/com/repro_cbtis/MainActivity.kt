e2bfc4b1d46f31f01cfec5ce5a1cd5980ae65a0c
AbrahamHub <apple_abraham@hotmail.com>
2021-05-19 12:32:17 -0500

    Iconos 🎶

--- app/src/main/java/com/repro_cbtis/MainActivity.kt
+++ app/src/main/java/com/repro_cbtis/MainActivity.kt
@@ -2,15 +2,21 @@ package com.repro_cbtis
 
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.repro_cbtis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var listMusic: ArrayList<Pair<String, String>>
+    private var reproduce = MediaPlayer()
+    private var song = -1
+    private var OnReproduce = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -32,6 +38,13 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 0 && permissions.contains(android.Manifest.permission.READ_EXTERNAL_STORAGE) && grantResults[0] == PackageManager.PERMISSION_GRANTED){
 
+        }else{
+            MaterialAlertDialogBuilder(this@MainActivity).setCancelable(false)
+                .setMessage(getString(R.string.permiso_requerido))
+                .setPositiveButton(resources.getString(R.string.aceptar)){_, _ ->
+                    finishAffinity()
+                }
+                .show()
         }
     }
 }
